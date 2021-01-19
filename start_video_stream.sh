6e94507350744c524387b97c797527b6b5492e75
EK <a@a.com>
2021-01-19 18:49:19 +0000

    added variables

--- start_video_stream.sh
+++ start_video_stream.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-raspivid -o - -t 0 -n -w 1280 -h 1024 -fps 60 -rot 180 | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264 --h264-fps=60 &
+
+FPS=30
+ROT=180
+
+raspivid -o - -t 0 -n -w 1280 -h 1024 -fps $FPS -rot $ROT | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream}' :demux=h264 --h264-fps=$FPS
