c0313c041ae427c0e7fa980f93f5e8e0d4baa0b7
EK <a@a.com>
2021-01-20 19:37:00 +0000

    updated FPS and resolution

--- start_video_stream_ffmpeg.sh
+++ start_video_stream_ffmpeg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
 source stream_keys.sh
 
-echo $TWITCH_STREAM_KEY
-
-FPS=25
+FPS=20
 ROT=180
 URL="rtmp://lhr04.contribute.live-video.net/app/"
 QUAL=20
 WIDTH=640
 HEIGHT=480
+
+# Group of Pictures (ideally 2x FPS)
+let GOP=$FPS*2
 
 if [ $ROT -eq 180 ]
 then
@@ -23,5 +23,5 @@ fi
 ffmpeg -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
  -f v4l2 -thread_queue_size 512 -codec:v h264 \
  -s "$WIDTH"x"$HEIGHT" -i /dev/video0 -codec:v copy -acodec aac -ab 128k \
- -r $FPS -g 50 -f flv "$URL""$TWITCH_STREAM_KEY"
+ -r $FPS -g $GOP -f flv "$URL""$TWITCH_STREAM_KEY"
 
