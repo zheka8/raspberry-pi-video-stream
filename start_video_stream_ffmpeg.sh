013f838acc231f972c45200ba8f0e784077f6588
EK <a@a.com>
2021-01-22 16:53:05 +0000

    added ampersand to background ffmpeg command

--- start_video_stream_ffmpeg.sh
+++ start_video_stream_ffmpeg.sh
@@ -12,5 +12,5 @@ v4l2-ctl --set-ctrl=rotate=$ROT
 ffmpeg -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
  -f v4l2 -thread_queue_size 512 -codec:v h264 -r $FPS \
  -s "$WIDTH"x"$HEIGHT" -i /dev/video0 -codec:v copy -acodec aac -ab 0k \
- -f flv "$URL""$TWITCH_STREAM_KEY"
+ -f flv "$URL""$TWITCH_STREAM_KEY" &
 
