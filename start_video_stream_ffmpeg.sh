f4814092e897b88ee552d380af4e163147ba5ec9
EK <a@a.com>
2021-01-22 14:41:43 +0000

    changed rotation to 270

--- start_video_stream_ffmpeg.sh
+++ start_video_stream_ffmpeg.sh
@@ -2,23 +2,12 @@
 source stream_keys.sh
 
 FPS=10
-ROT=180
+ROT=270
 URL="rtmp://lhr04.contribute.live-video.net/app/"
-#QUAL=20
 WIDTH=640
 HEIGHT=480
 
-# Group of Pictures (ideally 2x FPS)
-#let GOP=$FPS*2
-
-if [ $ROT -eq 180 ]
-then
-	v4l2-ctl --set-ctrl vertical_flip=1
-	v4l2-ctl --set-ctrl horizontal_flip=1
-else
-        v4l2-ctl --set-ctrl vertical_flip=0
-        v4l2-ctl --set-ctrl horizontal_flip=0
-fi
+v4l2-ctl --set-ctrl=rotate=$ROT
 
 ffmpeg -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
  -f v4l2 -thread_queue_size 512 -codec:v h264 -r $FPS \
