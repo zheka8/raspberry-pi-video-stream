2d56eaf1078b963f3f96753e88ac43ca0dd8cabb
EK <a@a.com>
2021-01-22 15:43:21 +0000

    updated rotation after reboot

--- start_video_stream_ffmpeg.sh
+++ start_video_stream_ffmpeg.sh
@@ -2,7 +2,7 @@
 source stream_keys.sh
 
 FPS=10
-ROT=270
+ROT=90
 URL="rtmp://lhr04.contribute.live-video.net/app/"
 WIDTH=640
 HEIGHT=480
@@ -11,6 +11,6 @@ v4l2-ctl --set-ctrl=rotate=$ROT
 
 ffmpeg -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero \
  -f v4l2 -thread_queue_size 512 -codec:v h264 -r $FPS \
- -s "$WIDTH"x"$HEIGHT" -i /dev/video0 -b:v 128k -codec:v copy -acodec aac -ab 0k \
+ -s "$WIDTH"x"$HEIGHT" -i /dev/video0 -codec:v copy -acodec aac -ab 0k \
  -f flv "$URL""$TWITCH_STREAM_KEY"
 
